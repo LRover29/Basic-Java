103c7724075e54778e3b9f8abe0efaf724686bd8
matiaselve4991 <matiaselve4991@gmail.com>
2023-08-13 11:42:11 +0800

    Add Liter Time

---
+++ cuConvertToLiter.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getVolume(25, 5, 2));
+    }
+
+    public static long getVolume(int a, int b, int c) {
+        return 1000L * a * b * c;
+    }
+}
