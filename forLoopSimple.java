671380e68891b303070fd2233d61f7763094fa4d
matiaselve4991 <matiaselve4991@gmail.com>
2023-08-13 16:56:40 +0800

    Additional java Basic Examples Loop

---
+++ forLoopSimple.java
@@ -0,0 +1,13 @@
+/* 
+Mercantile intentions
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I want a big salary, and that's why I'm studying Java");
+        }
+    }
+}
